f24a4b41c55fe19a5af8ae2d1a33dd5c5f53cc4a
Elsonsann <elson.1@hotmail.com>
2024-05-06 04:45:32 -0300

    Ola

---
+++ OlaOla/src/olaola/OlaOla.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package olaola;
+
+/**
+ *
+ * @author elson
+ */
+public class OlaOla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ola, Mundo");
+        System.out.println("\nOla, \nMundo");
+    }
+    
+}
